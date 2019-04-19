6e27d0a50e9a065f9f39f69b0170f1f9f197cfa5
CHANDRAKANTHREDDY93 <praneeth7516@gmail.com>
2019-04-18 22:52:39 -0400

    Modified server dist path

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/falcons-fantasy-ui'));
 
 app.listen(process.env.PORT || 8080);
 
