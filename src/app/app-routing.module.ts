1ccf9cc2f0ccf46299e3a26fe6f12e27f55ad6c6
CHANDRAKANTHREDDY93 <praneeth7516@gmail.com>
2019-04-23 00:00:53 -0400

    Added home view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { PointsSystemComponent } from './points-system/points-system.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'home', component: HomeComponent },
+  { path : 'leaderboard', component: LeaderboardComponent },
+  { path : 'pointsSystem', component: PointsSystemComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
