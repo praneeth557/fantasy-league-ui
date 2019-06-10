b0fef55cb81a12bac4a0e99e86df8671ee77feef
Praneeth <praneeth.karthikeya557@gmail.com>
2019-06-10 14:59:25 -0400

    Update app.constants.ts

--- src/app/constants/app.constants.ts
+++ src/app/constants/app.constants.ts
@@ -1,6 +1,6 @@
 export class AppConstants {
-  //APP_URL = "https://shrouded-sea-98380.herokuapp.com";
-  APP_URL = "http://localhost:8000";
+  APP_URL = "https://shrouded-sea-98380.herokuapp.com";
+  //APP_URL = "http://localhost:8000";
   GET_SECURITY_QUESTIONS_URL = "/api/user/getQuestions";
   GET_SECURITY_ANSWERS_URL = "/api/user/getAnswers";
   CREATE_USER_URL = "/api/user/create";
